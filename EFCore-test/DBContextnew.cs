f77b0e5452c6c4443687fc6afd744d342fc7e828
omranali2111 <aljabri1234@hotmail.com>
2023-09-18 12:08:50 +0400

    using EFCore to create database and to manage it

---
+++ EFCore-test/DBContextnew.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore_test
+{
+    internal class DBContextnew:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+
+            options.UseSqlServer("Data Source=(local);Initial Catalog=EFCore1; Integrated Security=true; TrustServerCertificate=True");
+        }
+    }
+}
