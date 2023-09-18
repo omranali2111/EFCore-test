4130862721b94c0b1d5fba7acb116aaa8c313ee8
omranali2111 <aljabri1234@hotmail.com>
2023-09-18 13:48:29 +0400

    insert data to db

--- EFCore-test/Program.cs
+++ EFCore-test/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EFCore_test;
+using Microsoft.EntityFrameworkCore;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using (var dbContext = new DBContextnew())
+        {
+            var newEmployee = new Employee
+            {
+                Name = "omran",
+                Salary = 1300
+            };
+            var newEmployee1 = new Employee
+            {
+                Name = "ali",
+                Salary = 1500
+            };
+
+            // Add the new Employee object to the DbSet
+            dbContext.employees.Add(newEmployee);
+            dbContext.employees.Add(newEmployee1);
+
+            // Save the changes to the database
+            dbContext.SaveChanges();
+
+          
+        }
+
+    }
+}
