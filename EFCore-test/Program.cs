fdd145c7c45da725400819adaf44399810cb2235
omranali2111 <aljabri1234@hotmail.com>
2023-09-18 14:15:55 +0400

    adding select and delete

--- EFCore-test/Program.cs
+++ EFCore-test/Program.cs
@@ -7,25 +7,52 @@ internal class Program
     {
         using (var dbContext = new DBContextnew())
         {
-            var newEmployee = new Employee
-            {
-                Name = "omran",
-                Salary = 1300
-            };
-            var newEmployee1 = new Employee
-            {
-                Name = "ali",
-                Salary = 1500
-            };
+            //var newEmployee = new Employee
+            //{
+            //    Name = "omran",
+            //    Salary = 1300
+            //};
+            //var newEmployee1 = new Employee
+            //{
+            //    Name = "ali",
+            //    Salary = 1500
+            //};
 
             // Add the new Employee object to the DbSet
-            dbContext.employees.Add(newEmployee);
-            dbContext.employees.Add(newEmployee1);
+            //dbContext.employees.Add(newEmployee);
+            //dbContext.employees.Add(newEmployee1);
 
             // Save the changes to the database
+            //dbContext.SaveChanges();
+
+            Employee em1 = dbContext.employees.Where(x => x.Id == 1).FirstOrDefault();
+
+            if (em1 != null)
+            {
+                Console.WriteLine("Employee found:");
+                Console.WriteLine($"Name: {em1.Name}");
+                Console.WriteLine($"Salary: {em1.Salary}");
+            }
+            else
+            {
+                Console.WriteLine("Employee with ID 1 not found.");
+            }
+
+            Employee em2 = dbContext.employees.First(x => x.Id == 4);
+
+            if (em2 != null)
+            {
+                dbContext.employees.Remove(em2);
+                dbContext.SaveChanges();
+                Console.WriteLine("Employee removed successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Employee with ID 4 or 3 not found.");
+            }
+
             dbContext.SaveChanges();
 
-          
         }
 
     }
